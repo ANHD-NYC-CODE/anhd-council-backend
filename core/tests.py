fe70c6bd3217e0f94528591c2c9b2e82301ffee7
0xstarcat <xxStarcat@gmail.com>
2018-12-29 23:04:52 -0500

    refactor seed_or_update and file tests

--- core/tests.py
+++ core/tests.py
@@ -1,7 +1,7 @@
 import os
 from django.test import TestCase
 from core.models import Update, Dataset, DataFile
-from datasets.models import Council, Building
+from datasets import models as ds_models
 from django.core.files import File
 from django.db import transaction, connection
 from core.utils.database import seed_whole_file_from_rows
@@ -28,29 +28,28 @@ class DatabaseTests(BaseTest, TestCase):
 
     def test_seed_whole_file_from_rows(self):
         dataset = Dataset.objects.create(name="mock", model_name="Building")
-        file = DataFile.objects.create(file=self.get_file('mock_csv.csv'), dataset=dataset)
+        file = DataFile.objects.create(file=self.get_file('test_pluto_17v1.zip'), dataset=dataset)
         update = Update.objects.create(model_name='Building', file=file)
-        rows = dataset.transform_dataset(self.get_file_path("test_pluto_17v1.zip"))
 
-        seed_whole_file_from_rows(Building, rows, update)
-        self.assertEqual(Building.objects.count(), 2)
+        ds_models.Building.seed_or_update_self(file=file, update=update)
+        self.assertEqual(ds_models.Building.objects.count(), 2)
         self.assertEqual(update.rows_created, 2)
 
     def test_seed_whole_file_from_rows_with_overwrite(self):
         dataset = Dataset.objects.create(name="mock", model_name="Building")
-        file = DataFile.objects.create(file=self.get_file('mock_csv.csv'), dataset=dataset)
-        council = Council.objects.create(coundist=1, geometry={})
-        Building.objects.create(bbl="2022600001", version="17v1", council=council,
-                                unitsres=1, unitstotal=1, yearbuilt=1900)
-        Building.objects.create(bbl="2022600004", version="17v1", council=council,
-                                unitsres=1, unitstotal=1, yearbuilt=1900)
+        file = DataFile.objects.create(file=self.get_file('test_pluto_18v1.zip'), dataset=dataset)
+        council = ds_models.Council.objects.create(coundist=1, geometry={})
+        ds_models.Building.objects.create(bbl="2022600001", version="17v1", council=council,
+                                          unitsres=1, unitstotal=1, yearbuilt=1900)
+        ds_models.Building.objects.create(bbl="2022600004", version="17v1", council=council,
+                                          unitsres=1, unitstotal=1, yearbuilt=1900)
 
         update = Update.objects.create(model_name='Building', file=file)
-        rows_18 = dataset.transform_dataset(self.get_file_path("test_pluto_18v1.zip"))
-        seed_whole_file_from_rows(Building, rows_18, update)
+
+        ds_models.Building.seed_or_update_self(file=file, update=update)
 
         self.assertEqual(update.rows_created, 1)
         self.assertEqual(update.rows_updated, 1)
-        self.assertEqual(Building.objects.count(), 3)
-        self.assertEqual(len(Building.objects.filter(version='17v1')), 1)
-        self.assertEqual(len(Building.objects.filter(version='18v1.1')), 2)
+        self.assertEqual(ds_models.Building.objects.count(), 3)
+        self.assertEqual(len(ds_models.Building.objects.filter(version='17v1')), 1)
+        self.assertEqual(len(ds_models.Building.objects.filter(version='18v1.1')), 2)
--- datasets/tests.py
+++ datasets/tests.py
@@ -1,8 +1,9 @@
 import os
 from django.test import TestCase
 from core.utils.database import seed_whole_file_from_rows
-from core.models import Dataset
-from datasets import models as dataset_models
+from core.models import Dataset, Update, DataFile
+from django_celery_results.models import TaskResult
+from datasets import models as ds_models
 from app.tests.base_test import BaseTest
 # Create your tests here.
 
@@ -13,10 +14,10 @@ class BuildingTests(BaseTest, TestCase):
 
     def test_seed_buildings(self):
         dataset = Dataset.objects.create(name="mock", model_name="Building")
-        rows = dataset.transform_dataset(self.get_file_path('test_pluto_17v1.zip'))
+        file = DataFile.objects.create(file=self.get_file('test_pluto_17v1.zip'), dataset=dataset)
 
-        seed_whole_file_from_rows(dataset_models.Building, rows)
-        self.assertEqual(dataset_models.Building.objects.count(), 2)
+        ds_models.Building.seed_or_update_self(file=file)
+        self.assertEqual(ds_models.Building.objects.count(), 2)
 
 
 class CouncilTests(BaseTest, TestCase):
@@ -25,19 +26,28 @@ class CouncilTests(BaseTest, TestCase):
 
     def test_seed_councils(self):
         dataset = Dataset.objects.create(name="mock", model_name="Council")
-        rows = dataset.transform_dataset(self.get_file_path("mock_council_json.geojson"))
+        file = DataFile.objects.create(file=self.get_file("mock_council_json.geojson"), dataset=dataset)
 
-        seed_whole_file_from_rows(dataset_models.Council, rows)
-        self.assertEqual(dataset_models.Council.objects.count(), 1)
+        ds_models.Council.seed_or_update_self(file=file)
+        self.assertEqual(ds_models.Council.objects.count(), 1)
 
 
 class HPDViolationTests(BaseTest, TestCase):
     def tearDown(self):
         self.clean_tests()
 
-    def test_seed_councils(self):
+    def test_seed_hpdviolation_first(self):
         dataset = Dataset.objects.create(name="mock", model_name="HPDViolation")
-        rows = dataset.transform_dataset(self.get_file_path("test_hpd_violations.csv"))
+        file = DataFile.objects.create(file=self.get_file('test_hpd_violations.csv'), dataset=dataset)
 
-        seed_whole_file_from_rows(dataset_models.HPDViolation, rows)
-        self.assertEqual(dataset_models.HPDViolation.objects.count(), 4)
+        ds_models.HPDViolation.seed_or_update_self(file=file)
+        self.assertEqual(ds_models.HPDViolation.objects.count(), 4)
+
+    def test_seed_hpdviolation_with_update(self):
+        dataset = Dataset.objects.create(name="mock", model_name="HPDViolation")
+        file = DataFile.objects.create(file=self.get_file('test_hpd_violations.csv'), dataset=dataset)
+        task_result = TaskResult.objects.create(status="SUCCESS", task_id="1")
+        update = Update.objects.create(dataset=dataset, model_name='HPDViolation', file=file, task_result=task_result)
+
+        ds_models.HPDViolation.seed_or_update_self(file=file)
+        self.assertEqual(ds_models.HPDViolation.objects.count(), 4)
