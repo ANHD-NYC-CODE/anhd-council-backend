279501198e6bba00dee75190b51cfad087c2b629
0xstarcat <xxStarcat@gmail.com>
2019-01-12 19:27:41 -0500

    add a query test for related Legacy and Now buildings

--- datasets/models/DOBPermitIssuedLegacy.py
+++ datasets/models/DOBPermitIssuedLegacy.py
@@ -113,4 +113,4 @@ class DOBPermitIssuedLegacy(BaseDatasetModel, models.Model):
         return self.seed_or_update_from_set_diff(**kwargs)
 
     def __str__(self):
-        return str(self.violationid)
+        return str(self.job)
