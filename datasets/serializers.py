c13ec9cc18d021ee5e9ca803dddc8e1149efb42c
0xstarcat <xxStarcat@gmail.com>
2019-01-21 00:23:03 -0500

    add django-restframework serializers and views for council

--- datasets/serializers.py
+++ datasets/serializers.py
@@ -3,15 +3,29 @@ from datasets import models as ds
 from django.forms.models import model_to_dict
 
 
-def council_housing_type_dict(councilnum):
-    council_buildings = ds.Property.objects.council(councilnum)
-    return {
-        "buildings_count": council_buildings.count(),
-        "rent_stabilized_count": council_buildings.rentstab().count(),
-        "rent_regulated_count": council_buildings.rentreg().count(),
-        "small_homes_count": council_buildings.smallhome().count(),
-        "market_rate_count": council_buildings.marketrate().count(),
-    }
+class CouncilSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ds.Council
+        fields = '__all__'
+
+
+class CouncilDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ds.Council
+        fields = '__all__'
+
+    housing_types = serializers.SerializerMethodField()
+
+    def get_housing_types(self, obj):
+        council_buildings = ds.Property.objects.council(obj.pk)
+
+        return {
+            "buildings_count": council_buildings.count(),
+            "rent_stabilized_count": council_buildings.rentstab().count(),
+            "rent_regulated_count": council_buildings.rentreg().count(),
+            "small_homes_count": council_buildings.smallhome().count(),
+            "market_rate_count": council_buildings.marketrate().count(),
+        }
 
 
 def property_query_serializer(properties):
--- datasets/views.py
+++ datasets/views.py
@@ -1,12 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from django.http import JsonResponse, HttpResponseNotFound
 from django.core import serializers
 from django.conf import settings
 from django.core.cache import cache
 from datasets.helpers.api_helpers import queryset_by_housingtype
-from datasets.serializers import council_housing_type_dict, property_query_serializer, property_lookup_serializer
+from datasets.serializers import property_query_serializer, property_lookup_serializer
 from datasets.models.Property import PropertyFilter
+from datasets import serializers as serial
+
 
 from datasets import models as ds
 import logging
@@ -14,40 +18,48 @@ import json
 logger = logging.getLogger('app')
 
 
-@api_view(['GET'])
-def councils_index(request, format=None):
-    if request.path_info in cache:
-        logger.debug('Serving cache: {}'.format(request.path_info))
-        councils_json = cache.get(request.path_info)
-        return JsonResponse(councils_json, safe=False)
+def council_list(request):
+    if request.method == 'GET':
+        if request.path_info in cache:
+            logger.debug('Serving cache: {}'.format(request.path_info))
+            return JsonResponse(cache.get(request.path_info), safe=False)
+        else:
+            try:
+                councils = ds.Council.objects.all()
+            except ds.Council.DoesNotExist:
+                return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+            try:
+                serializer = serial.CouncilSerializer(councils, many=True)
+                logger.debug('Caching: {}'.format(request.path_info))
+                cache.set(request.path_info, serializer.data, timeout=settings.CACHE_TTL)
+                return JsonResponse(serializer.data, safe=False)
+            except Exception as e:
+                return JsonResponse(e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR, safe=False)
     else:
-        try:
-            councils = ds.Council.objects.all()
-            councils_json = serializers.serialize('json', councils)
-            logger.debug('Caching: {}'.format(request.path_info))
-            cache.set(request.path_info, councils_json, timeout=settings.CACHE_TTL)
-            return JsonResponse(councils_json, safe=False)
-        except Exception as e:
-            return JsonResponse(e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR, safe=False)
+        return HttpResponse(400)
 
 
-@api_view(['GET'])
-def council_show(request, councilnum, format=None):
-    if request.path_info in cache:
-        logger.debug('Serving cache: {}'.format(request.path_info))
-        council_json = cache.get(request.path_info)
-        return JsonResponse(council_json, safe=False)
+def council_detail(request, pk):
+    if request.method == 'GET':
+        if request.path_info in cache:
+            logger.debug('Serving cache: {}'.format(request.path_info))
+            return JsonResponse(cache.get(request.path_info), safe=False)
+        else:
+            try:
+                council = ds.Council.objects.get(pk=pk)
+            except ds.Council.DoesNotExist:
+                return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+            try:
+                serializer = serial.CouncilDetailSerializer(council)
+                logger.debug('Caching: {}'.format(request.path_info))
+                cache.set(request.path_info, serializer.data, timeout=settings.CACHE_TTL)
+                return JsonResponse(serializer.data, safe=False)
+            except Exception as e:
+                return JsonResponse(e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR, safe=False)
     else:
-        try:
-            council = ds.Council.objects.get(coundist=councilnum)
-            council_json = json.dumps(council_housing_type_dict(council.coundist))
-            logger.debug('Caching: {}'.format(request.path_info))
-            cache.set(request.path_info, council_json, timeout=settings.CACHE_TTL)
-            return JsonResponse(council_json, safe=False)
-        except ds.Council.DoesNotExist as e:
-            return JsonResponse([], status=status.HTTP_404_NOT_FOUND, safe=False)
-        except Exception as e:
-            return JsonResponse(e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR, safe=False)
+        return HttpResponse(400)
 
 
 @api_view(['GET'])
