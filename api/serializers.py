189c540b1b1655a0c0aefe408c7addf824fa69fd
0xstarcat <xxStarcat@gmail.com>
2019-01-20 23:38:01 -0500

    add property lookup query

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from datasets import models as ds
+from django.forms.models import model_to_dict
 
 
 def council_housing_type_dict(councilnum):
@@ -13,5 +14,44 @@ def council_housing_type_dict(councilnum):
     }
 
 
-def property_serializer(properties):
+def property_query_serializer(properties):
     return list({'foo': 'bar'} for property in properties)
+
+
+def property_lookup_serializer(property):
+    property_dict = model_to_dict(property)
+    buildings = property.building_set.all()
+    hpdcomplaints = property.hpdcomplaint_set.all()
+    hpdviolations = property.hpdviolation_set.all()
+
+    def get_house_number(building):
+        if (building.lhnd == building.hhnd):
+            return building.lhnd
+        elif (building.lhnd and building.hhnd):
+            return "{}-{}".format(building.lhnd, building.hhnd)
+        else:
+            return building.lhnd
+
+    property_dict["buildings"] = {
+        "total": buildings.count(),
+        "items": list({
+            "bin": building.bin,
+            "house_number": get_house_number(building)
+        } for building in buildings)
+    }
+
+    property_dict["hpdcomplaints"] = {
+        "total": hpdcomplaints.count(),
+        "items": [
+            model_to_dict(hpdcomplaint) for hpdcomplaint in hpdcomplaints
+        ]
+    }
+
+    property_dict["hpdviolations"] = {
+        "total": hpdviolations.count(),
+        "items": [
+            model_to_dict(hpdviolation) for hpdviolation in hpdviolations
+        ]
+    }
+
+    return property_dict
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -2,8 +2,8 @@ from django.test import TestCase, RequestFactory
 from app.tests.base_test import BaseTest
 
 
-from api.views import councils_index
-
+from api.views import councils_index, property_lookup
+import json
 import logging
 logging.disable(logging.CRITICAL)
 
@@ -20,3 +20,35 @@ class CouncilsIndexTests(BaseTest, TestCase):
         request = self.factory.get('/councils')
         response = councils_index(request)
         self.assertEqual(response.status_code, 200)
+
+
+class PropertyLookupTests(BaseTest, TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def tearDown(self):
+        self.clean_tests()
+
+    def test_api_request(self):
+        council = self.council_factory(coundist=1)
+        property = self.property_factory(council=council, bbl="1", yearbuilt="1910")
+        building1 = self.building_factory(property=property, bin="10a", lhnd="100", hhnd="100")
+        building2 = self.building_factory(property=property, bin="10b", lhnd="102", hhnd="102")
+        hpdbuilding = self.hpdbuilding_factory(property=property, building=building1)
+
+        self.hpdcomplaint_factory(property=property, hpdbuilding=hpdbuilding, status="ACTIVE")
+        self.hpdviolation_factory(property=property, building=building1)
+
+        request = self.factory.get('/buildings/1')
+        response = property_lookup(request, "1")
+        content = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content['bbl'], '1')
+        self.assertEqual(content['yearbuilt'], 1910)
+        self.assertEqual(content['buildings']['total'], 2)
+        self.assertEqual(content['buildings']['items'][0]['bin'], '10b')
+        self.assertEqual(content['buildings']['items'][1]['house_number'], '100')
+        self.assertEqual(content['hpdcomplaints']['total'], 1)
+        self.assertEqual(content['hpdcomplaints']['items'][0]["status"], "ACTIVE")
+        self.assertEqual(content['hpdviolations']['total'], 1)
+        self.assertEqual(content['hpdviolations']['items'][0]["currentstatus"], "ACTIVE")
