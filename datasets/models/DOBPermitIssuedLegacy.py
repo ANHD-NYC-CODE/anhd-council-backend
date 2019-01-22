39a510b1cc589c176162e325d6df4d9baff6065d
0xstarcat <xxStarcat@gmail.com>
2019-01-22 17:37:07 -0500

    udpate testrs

--- datasets/models/DOBPermitIssuedLegacy.py
+++ datasets/models/DOBPermitIssuedLegacy.py
@@ -112,7 +112,7 @@ class DOBPermitIssuedLegacy(BaseDatasetModel, models.Model):
     @classmethod
     def seed_or_update_self(self, **kwargs):
         logger.debug("Seeding/Updating {}", self.__name__)
-        return self.bulk_seed(**kwargs, overwrite=True)
+        return self.seed_or_update_from_set_diff(**kwargs)
 
     def __str__(self):
         return str(self.job)
