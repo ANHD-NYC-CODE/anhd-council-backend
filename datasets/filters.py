6d532803a44d09d648b2c9ca0618a7066672bcc8
0xstarcat <xxStarcat@gmail.com>
2019-01-22 21:07:10 -0500

    add some filters

--- datasets/filters.py
+++ datasets/filters.py
@@ -1,10 +1,22 @@
 from datasets import models as ds
-from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework import FilterSet, NumberFilter
+from django.db.models import Count
 
 
 class PropertyFilter(FilterSet):
+    hpdviolations = NumberFilter(method='filter_hpdviolations_exact')
+    hpdviolations__gt = NumberFilter(method='filter_hpdviolations_gt')
+
+    def filter_hpdviolations_exact(self, queryset, name, value):
+        return queryset.annotate(hpdviolations=Count('hpdviolation', distinct=True)).filter(hpdviolations=value)
+
+    def filter_hpdviolations_gt(self, queryset, name, value):
+        return queryset.annotate(hpdviolations=Count('hpdviolation', distinct=True)).filter(hpdviolations__gt=value)
+
     class Meta:
         model = ds.Property
         fields = {
             'yearbuilt': ['exact', 'lt', 'gt'],
+            'council': ['exact']
+
         }
