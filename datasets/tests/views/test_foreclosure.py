359b21e952387110d98fe9311fb3c666d5a5ce21
0xstarcat <xxStarcat@gmail.com>
2019-02-04 01:33:50 -0500

    add authorized tests for foreclosures

--- datasets/tests/views/test_foreclosure.py
+++ datasets/tests/views/test_foreclosure.py
@@ -11,12 +11,37 @@ class ForeclosureViewTests(BaseTest, TestCase):
     def tearDown(self):
         self.clean_tests()
 
-    def test_list(self):
+    def test_unauthorized_list(self):
         response = self.client.get('/foreclosures/', format="json")
 
         self.assertEqual(response.status_code, 401)
 
-    def test_retrieve(self):
+    def test_unauthorized_retrieve(self):
         response = self.client.get('/foreclosures/1/')
 
         self.assertEqual(response.status_code, 401)
+
+    def test_list(self):
+        self.foreclosure_factory()
+        self.foreclosure_factory()
+
+        token = self.get_access_token()
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        response = self.client.get('/foreclosures/', format="json")
+        content = response.data['results']
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(content), 2)
+
+    def test_retrieve(self):
+        self.foreclosure_factory(key="1")
+        token = self.get_access_token()
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        response = self.client.get('/foreclosures/1/')
+
+        content = response.data
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content["key"], "1")
