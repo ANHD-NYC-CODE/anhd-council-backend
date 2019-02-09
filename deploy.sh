063bf875e0e9f692e65b4ed93c40e1a6b2dcc03b
0xstarcat <xxStarcat@gmail.com>
2019-02-09 11:02:52 -0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 cd /var/www/anhd-council-backend
 git pull origin master
-pipenv install
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
+docker-compose rebuild app
