c552671ca6ceee310d8a70c4e8e0f15882bccc97
0xstarcat <xxStarcat@gmail.com>
2019-02-10 11:07:54 -0500

    update tasks

--- core/utils/database.py
+++ core/utils/database.py
@@ -13,7 +13,6 @@ import math
 import os
 import json
 import logging
-import shutil
 
 logger = logging.getLogger('app')
 
@@ -41,9 +40,6 @@ def seed_from_csv_diff(original_file_path, new_file_path, model, **kwargs):
     seeds Diff Set() in batches
 
     """
-
-    # Make sure temp dir is empty first
-    # shutil.rmtree(settings.MEDIA_TEMP_ROOT)
 
     original_diff_set = set()
     new_diff_set = set()
