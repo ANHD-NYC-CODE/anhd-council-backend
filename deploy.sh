856637656f4376e6e587f86cdfaf1185312163c7
0xstarcat <xxStarcat@gmail.com>
2019-02-09 22:12:17 -0500

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 cd /var/www/anhd-council-backend
 git pull origin master
-docker-compose rebuild app
+
+# Need to restart celery workers
+docker-compose restart celery_default
+docker-compose restart celery_update
+docker-compose restart celerybeat
