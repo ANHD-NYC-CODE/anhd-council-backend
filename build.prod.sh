5bd7dcd43f8fd8516b0122de91e9813f6b4d3057
0xstarcat <xxStarcat@gmail.com>
2019-02-09 23:05:05 -0500

    deploy

--- build.prod.sh
+++ build.prod.sh
@@ -1,3 +1,5 @@
+# may need to run as sudo
+# sudo sh build.prod.sh
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 echo "Produdction build complete!"
