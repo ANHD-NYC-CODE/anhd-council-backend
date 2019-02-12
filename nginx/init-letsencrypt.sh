ccc9c2585db7b5083d6516084fa69351257afea2
0xstarcat <xxStarcat@gmail.com>
2019-02-12 11:16:28 -0500

    nginx https

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-domains=(api.displacementalert.org tasks.displacementalert.org)
+domains=(api.displacementalert.org)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="anhd.tech@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
