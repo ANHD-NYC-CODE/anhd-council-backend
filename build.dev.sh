f32045358dd018bd5a9c6533392888d7f1119335
0xstarcat <xxStarcat@gmail.com>
2019-02-13 10:07:03 -0500

    update deploy scripts

--- build.dev.sh
+++ build.dev.sh
@@ -1,3 +1,3 @@
 # sh build.dev.sh
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --force-recreate --build
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --force-recreate --build --remove-orphans
 echo "Dev build complete!"
--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate --build nginx
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate --build --remove-orphans nginx
