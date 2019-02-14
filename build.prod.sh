92915a4e2ab88cffbd1cd7d0f64465f5a734ee6e
0xstarcat <xxStarcat@gmail.com>
2019-02-13 22:32:25 -0500

    update

--- build.prod.sh
+++ build.prod.sh
@@ -1,4 +1,5 @@
 # may need to run as sudo
 # sudo sh build.prod.sh
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate --build --remove-orphans
+
 echo "Production build complete!"
