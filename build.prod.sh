154a62a85737b1198509c940cfb45d743c7c172e
0xstarcat <xxStarcat@gmail.com>
2019-02-14 23:31:15 -0500

    migrate to build scripts

--- build.prod.sh
+++ build.prod.sh
@@ -1,5 +1,6 @@
 # may need to run as sudo
 # sudo sh build.prod.sh
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate --build --remove-orphans
+docker exec -it app python manage.py migrate
 
 echo "Production build complete!"
