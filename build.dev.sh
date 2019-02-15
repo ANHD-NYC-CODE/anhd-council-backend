45fb18ddc14d61f20ed4d1f880195f1665a970dd
0xstarcat <xxStarcat@gmail.com>
2019-02-15 09:47:07 -0500

    add migration

--- build.dev.sh
+++ build.dev.sh
@@ -1,5 +1,5 @@
 # sh build.dev.sh
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --force-recreate --build --remove-orphans
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --force-recreate --build --remove-orphans
 docker exec -it app python manage.py migrate
 
 echo "Dev build complete!"
