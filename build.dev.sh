135757a487aa836c855a253e8092a750a3b9697e
0xstarcat <xxStarcat@gmail.com>
2019-02-21 13:34:26 -0500

    update builds

--- build.dev.sh
+++ build.dev.sh
@@ -1,6 +1,6 @@
 # sh build.dev.sh
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --force-recreate --build --remove-orphans
 docker exec -it app python manage.py migrate
-docker image prune
+docker image prune -f
 
 echo "Dev build complete!"
