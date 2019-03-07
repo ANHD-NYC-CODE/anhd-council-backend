509af01608ef8921e68998a420b3de7815bbc3bd
0xstarcat <xxStarcat@gmail.com>
2019-03-06 23:28:23 -0500

    more timeout for gunicorn server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,4 +11,4 @@ python3.6 manage.py collectstatic --noinput
 
 # Start server
 echo "Starting server"
-/usr/local/bin/gunicorn app.wsgi:application -w 2 -b :8000 --reload
+/usr/local/bin/gunicorn app.wsgi:application -w 5 -t 60 -b :8000 --reload --graceful-timeout 30
