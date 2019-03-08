4d6a2d44743d28126ac5352b83f615b8cdcf836d
0xstarcat <xxStarcat@gmail.com>
2019-03-08 10:51:38 -0500

    increase gunicorn timeout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,4 +11,4 @@ python3.6 manage.py collectstatic --noinput
 
 # Start server
 echo "Starting server"
-/usr/local/bin/gunicorn app.wsgi:application -w 5 -t 120 -b :8000 --reload --graceful-timeout 30
+/usr/local/bin/gunicorn app.wsgi:application -w 5 -t 480 -b :8000 --reload --graceful-timeout 30
