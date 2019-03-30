5ec160dfbc9672c645bc396a32365f13ca412744
0xstarcat <xxStarcat@gmail.com>
2019-03-30 18:37:38 -0400

    modify deploy scripts

--- build.prod.sh
+++ build.prod.sh
@@ -3,3 +3,6 @@ docker exec -it app python manage.py migrate
 sudo docker exec -it redis redis-cli FLUSHALL
 docker image prune -f
 echo "Production build complete!"
+
+echo "starting pre-cache"
+docker exec -it app python pre_cache.py
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,1 @@
 ssh -t anhd@45.55.44.160 "cd /var/www/anhd-council-backend && sudo sh pull.sh"
-
-docker exec -it app python pre_cache.py
