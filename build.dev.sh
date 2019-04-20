c9c6546d2c3eafb4e3134cc2c9cb872a392c7675
0xstarcat <xxStarcat@gmail.com>
2019-04-20 15:36:41 -0400

    build app_image

--- build.dev.sh
+++ build.dev.sh
@@ -1,5 +1,5 @@
 # sh build.dev.sh
-# docker build -f Dockerfile --tag app_image .
+docker build -f Dockerfile --tag app_image .
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --force-recreate --build --remove-orphans
 docker exec -it app python manage.py migrate
 docker image prune -f
--- build.prod.sh
+++ build.prod.sh
@@ -1,3 +1,4 @@
+docker build -f Dockerfile --tag app_image .
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate --build --remove-orphans
 docker exec -it app python manage.py migrate
 sudo docker exec -it redis redis-cli FLUSHALL
