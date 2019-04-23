0b2047e229c9a704579a0088fd379eb2cff57c82
0xstarcat <xxStarcat@gmail.com>
2019-04-22 23:41:07 -0400

    update cache

--- app/tasks.py
+++ app/tasks.py
@@ -56,7 +56,7 @@ def clean_database():
         realconn.set_isolation_level(0)
         cursor = realconn.cursor()
         cursor.execute('VACUUM ANALYZE')
-        cursor.execute('REINDEX SYSTEM anhd')
+        cursor.execute('REINDEX DATABASE anhd')
         realconn.set_isolation_level(old_isolation_level)
     except Exception as e:
         logger.error('Error during task: {}'.format(e))
--- core/utils/cache.py
+++ core/utils/cache.py
@@ -2,6 +2,8 @@ import requests
 import logging
 
 logger = logging.getLogger('app')
+import datetime
+from dateutil.relativedelta import relativedelta
 
 
 def create_async_cache_workers():
@@ -13,24 +15,61 @@ def create_async_cache_workers():
 
 def cache_council_property_summaries():
     from datasets.models import Council
+    today = datetime.datetime.today()
+    one_month_ago = (today.replace(day=1) - relativedelta(months=1)).strftime('%m/%d/%Y')
+    one_year_ago = (today.replace(day=1) - relativedelta(years=1)).strftime('%m/%d/%Y')
+    three_years_ago = (today.replace(day=1) - relativedelta(years=1)).strftime('%m/%d/%Y')
 
+    # cache 1 month
     for record in Council.objects.all():
         print("Caching Council: {}".format(record.pk))
         logger.debug("Caching Council: {}".format(record.pk))
         requests.get(
-            'https://api.displacementalert.org/councils/{}/properties/?format=json&summary=true&summary-type=short'.format(record.pk))
+            'https://api.displacementalert.org/councils/{}/properties/?format=json&summary=true&summary-type=short-annotated&annotation__start={}&unitsres__gte=1'.format(record.pk, one_month_ago))
+
+    # cache 1 year
+    for record in Council.objects.all():
+        print("Caching Council: {}".format(record.pk))
+        logger.debug("Caching Council: {}".format(record.pk))
+        requests.get(
+            'https://api.displacementalert.org/councils/{}/properties/?format=json&summary=true&summary-type=short-annotated&annotation__start={}&unitsres__gte=1'.format(record.pk, one_year_ago))
+
+    # cache 3 year
+    for record in Council.objects.all():
+        print("Caching Council: {}".format(record.pk))
+        logger.debug("Caching Council: {}".format(record.pk))
+        requests.get(
+            'https://api.displacementalert.org/councils/{}/properties/?format=json&summary=true&summary-type=short-annotated&annotation__start={}&unitsres__gte=1'.format(record.pk, three_years_ago))
     print("Council Pre-Caching complete!")
     logger.debug("Council Pre-Caching complete!")
 
 
 def cache_community_property_summaries():
     from datasets.models import Community
+    today = datetime.datetime.today()
+    one_month_ago = (today.replace(day=1) - relativedelta(months=1)).strftime('%m/%d/%Y')
+    one_year_ago = (today.replace(day=1) - relativedelta(years=1)).strftime('%m/%d/%Y')
+    three_years_ago = (today.replace(day=1) - relativedelta(years=1)).strftime('%m/%d/%Y')
 
     for record in Community.objects.all():
         print("Caching Community: {}".format(record.pk))
         logger.debug("Caching Community: {}".format(record.pk))
         requests.get(
-            'https://api.displacementalert.org/communities/{}/properties/?format=json&summary=true&summary-type=short'.format(record.pk))
+            'https://api.displacementalert.org/communities/{}/properties/?format=json&summary=true&summary-type=short-annotated&annotation__start={}&unitsres__gte=1'.format(record.pk, one_month_ago))
+
+    # cache 1 year
+    for record in Community.objects.all():
+        print("Caching Community: {}".format(record.pk))
+        logger.debug("Caching Community: {}".format(record.pk))
+        requests.get(
+            'https://api.displacementalert.org/communities/{}/properties/?format=json&summary=true&summary-type=short-annotated&annotation__start={}&unitsres__gte=1'.format(record.pk, one_year_ago))
+
+    # cache 3 year
+    for record in Community.objects.all():
+        print("Caching Community: {}".format(record.pk))
+        logger.debug("Caching Community: {}".format(record.pk))
+        requests.get(
+            'https://api.displacementalert.org/communities/{}/properties/?format=json&summary=true&summary-type=short-annotated&annotation__start={}&unitsres__gte=1'.format(record.pk, three_years_ago))
 
     print("Community Pre-Caching complete!")
     logger.debug("Community Pre-Caching complete!")
