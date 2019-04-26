34c87a369e3f8c6e8b1826ddea6fee4dec2152ee
0xstarcat <xxStarcat@gmail.com>
2019-04-26 09:34:46 -0400

    cache

--- begin_cache.sh
+++ begin_cache.sh
@@ -1,1 +1,1 @@
-ssh -t anhd@45.55.44.160 "cd /var/www/anhd-council-backend && sudo docker exec -it redis redis-cli FLUSHALL && sudo docker exec -it app python pre_cache.py"
+ssh -t anhd@45.55.44.160 "cd /var/www/anhd-council-backend && sudo docker exec -it app python pre_cache.py"
