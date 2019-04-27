f6c5ec9b2fac5bb793ec0653062e848f8fb7a006
0xstarcat <xxStarcat@gmail.com>
2019-04-27 11:05:09 -0400

    change Acris sale types to just 'deed'

--- datasets/models/AcrisRealMaster.py
+++ datasets/models/AcrisRealMaster.py
@@ -47,11 +47,12 @@ class AcrisRealMaster(BaseDatasetModel, models.Model):
     goodthroughdate = models.DateTimeField(blank=True, null=True)
 
     # https://data.cityofnewyork.us/City-Government/ACRIS-Document-Control-Codes/7isb-wh4c
-    SALE_DOC_TYPES = ("DEED", "DEEDO", "DEED, LE", "DEED, RC", "DEED, TS", "DEEDP", "MTGE",
-                      "SPRD", "M&CON")
+    # SALE_DOC_TYPES = ("DEED", "DEEDO", "DEED, LE", "DEED, RC", "DEED, TS", "DEEDP", "MTGE",
+    #                   "SPRD", "M&CON")
     LEASE_DOC_TYPES = ("LEAS", "ASSTO", "MLEA1")
 
     TAX_DOC_TYPES = ("RPTT")
+    SALE_DOC_TYPES = ("DEED")
 
     @classmethod
     def create_async_update_worker(self):
