86b1e39c11502f9abf896207c01cd92fae1feeb1
0xstarcat <xxStarcat@gmail.com>
2019-04-28 13:44:31 -0400

    fix tasks

--- app/tasks.py
+++ app/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from app.celery import app
 import shutil
-from core.tasks import async_send_update_error_mail
+from core.tasks import async_send_general_task_error_mail
 from rest_framework_simplejwt.token_blacklist.management.commands import flushexpiredtokens
 from django.core import cache
 from core.utils.cache import create_async_cache_workers
