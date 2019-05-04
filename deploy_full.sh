dd7c3502afd28253df146ec852ee94e0fccf9eef
0xstarcat <xxStarcat@gmail.com>
2019-05-04 11:58:06 -0400

    update annotation task

--- deploy_full.sh
+++ deploy_full.sh
@@ -1,1 +1,1 @@
-ssh -t anhd@45.55.44.160 "cd /var/www/anhd-council-backend && sudo sh pull.sh"
+ssh -t anhd@45.55.44.160 "cd /var/www/anhd-council-backend && sudo git pull origin master && sudo sh recreate.prod.sh"
