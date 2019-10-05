7a4e62fccb5fbbb0fa0521c5fab54292c91b2c87
0xstarcat <xxStarcat@gmail.com>
2019-10-04 22:05:42 -0400

    add staging nginx config

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-domains=(api.displacementalert.org)
+domains=(staging.portal.displacementalert.org)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="anhd.tech@gmail.com" # Adding a valid address is strongly recommended
