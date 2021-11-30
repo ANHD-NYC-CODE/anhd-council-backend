9e10f0eeaa0c65a14c918c24e2f1454451f13344
tim_jackins <jack@jacktimmins.com>
2021-11-30 03:38:22 -0500

    Change OCA issue to 409

--- datasets/views/ocahousingcourt_views.py
+++ datasets/views/ocahousingcourt_views.py
@@ -4,8 +4,8 @@ from rest_framework.settings import api_settings
 from rest_framework_csv import renderers as rf_csv
 from datasets.helpers.cache_helpers import cache_request_path
 from datasets.helpers.api_helpers import ApplicationViewSet
-from django.http import HttpResponseForbidden
-
+from rest_framework.response import Response
+from rest_framework import status
 
 from datasets import serializers as serial
 from datasets import models as ds
@@ -33,7 +33,7 @@ class OCAHousingCourtViewSet(ApplicationViewSet, NestedViewSetMixin, viewsets.Re
             response = super().list(request, *args, **kwargs)
             return response
         except Exception as e:
-            return HttpResponseForbidden('OCA Housing Court data only available for properties with more than 10 units')
+            return Response('OCA Housing Court data only available for properties with more than 10 units', status=status.HTTP_409_CONFLICT)
 
 
     @cache_request_path()
@@ -44,4 +44,4 @@ class OCAHousingCourtViewSet(ApplicationViewSet, NestedViewSetMixin, viewsets.Re
             response = super().retrieve(request, *args, **kwargs)
             return response
         except Exception as e:
-            return HttpResponseForbidden('OCA Housing Court data only available for properties with more than 10 units')
+            return Response('OCA Housing Court data only available for properties with more than 10 units', status=status.HTTP_409_CONFLICT)
