fe2161043e3302fff6969b9d033774362dd20a0a
tim_jackins <jack@jacktimmins.com>
2021-12-04 14:27:16 -0500

    Email copy

--- app/tasks.py
+++ app/tasks.py
@@ -162,7 +162,7 @@ def async_send_user_notification_email(self, user_id, save_name, save_url, new_r
     subject = 'Custom Search Updated'
     content = f'<h3>Hello {user.username}!</h3>'
 
-    content += f'<p>Your saved custom search, "{save_name}." has new results.'
+    content += f'<p>Your saved custom search, "{save_name}" has new results.'
 
     if new_result_num > 0 and len(new_results_url) > 0:
         content += f'<p>There are {new_result_num} new results since the last notification on {last_notified_date}. To view new results <a href="{new_results_url}">Click here</a></p>'
