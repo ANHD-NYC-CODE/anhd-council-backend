27dcca57be28c3a2c93216986353295aa30d78df
Scott Kutler <scottkutler@gmail.com>
2022-11-10 15:26:29 -0500

    property file updates

--- datasets/models/Property.py
+++ datasets/models/Property.py
@@ -439,13 +439,13 @@ class Property(BaseDatasetModel, models.Model):
     @classmethod
     def add_state_geographies(self):
         logger.info('Adding State Assembly associations via geoshape')
-        status1 = self.queryset_foreach(self.objects.filter(stateassembly__isnull=True, latitude__isnull=False,
+        status1 = self.queryset_foreach(self.objects.filter(latitude__isnull=False,
                                                             longitude__isnull=False).order_by('pk'), self.create_state_assembly_association, 'stateassembly', batch_size=1000)
 
         logger.info(status1)
 
         logger.info('Adding State Senate associations via geoshape')
-        status2 = self.queryset_foreach(self.objects.filter(statesenate__isnull=False, latitude__isnull=False,
+        status2 = self.queryset_foreach(self.objects.filter(latitude__isnull=False,
                                                             longitude__isnull=False).order_by('pk'), self.create_state_senate_association, 'statesenate', batch_size=1000)
         logger.info(status2)
 
