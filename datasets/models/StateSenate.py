4335b0f734968d4f2cdb4ea12d00e236b8a0d732
Scott Kutler <scottkutler@gmail.com>
2022-11-30 17:50:07 -0500

    added notes on how to update zipcodes and senates

--- datasets/models/StateSenate.py
+++ datasets/models/StateSenate.py
@@ -9,9 +9,13 @@ from core.utils.transform import from_geojson
 #
 # Copy data from:
 # http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nyss/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson
-# Paste into a .geojson file, upload file through admin, update
+# Paste into a .geojson file, upload file through admin, update.
 
-# As a note, a property will NOT currently update its state senate, assembly, etc unless it doesn't currently have one ('null') or is a new property. I assume it was coded this way because it takes 2-3 seconds to calculate each state senate, etc, based on its geoshape location and long/latitude. To update in 2022, I null'd each  state senate entry as I updated the properties to ensure they updated.
+# Second Step: After updating the dataset, make sure to update or create a new map on https://studio.mapbox.com/ by uploading the shapefile dataset/zipfiles and then update the api link on the front end app "/src/LeafletMap/index.js" for the zipcode api with the new mapbox map.
+
+# Make sure to clear cache after. If you see overlapping layers or old dataset data still even after updating, please make sure your browser cache is cleared or test in a private browser.
+
+# As a note, a property will NOT currently update its state senate, assembly, etc unless it doesn't currently have one ('null') or is a new property. I assume it was coded this way because it takes 2-3 seconds to calculate each state senate, etc, based on its geoshape location and long/latitude. To update in 2022, I null'd each  state senate entry as I updated the properties to ensure they updated. The entire process took about 4-5 days to recalculate each properties new District based on it's map location.
 
 class StateSenate(BaseDatasetModel, models.Model):
     id = models.IntegerField(primary_key=True, blank=False, null=False)
