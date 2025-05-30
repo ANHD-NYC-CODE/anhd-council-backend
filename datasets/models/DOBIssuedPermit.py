3f1181c771cc49f8a2c5c6541331fb136e33769b
Scott Kutler <scottkutler@gmail.com>
2025-05-30 12:11:12 -0400

    fix dob now updates

--- datasets/models/DOBIssuedPermit.py
+++ datasets/models/DOBIssuedPermit.py
@@ -79,7 +79,7 @@ class DOBIssuedPermit(BaseDatasetModel, models.Model):
             [k.name + "=EXCLUDED." + k.name for k in self._meta.get_fields()])
 
         if raw_select:
-            sql = "INSERT INTO {table_name} ({fields}) SELECT {cols} FROM {other_table_name} ON CONFLICT ({primary_key}) DO UPDATE SET {upsert_fields};"
+            sql = "INSERT INTO {table_name} ({fields}) {cols} ON CONFLICT ({primary_key}) DO UPDATE SET {upsert_fields};"
         else:
             sql = "INSERT INTO {table_name} ({fields}) SELECT {cols} FROM {other_table_name} ON CONFLICT ({primary_key}) DO UPDATE SET {upsert_fields};"
         return sql.format(table_name=table_name, fields=fields, cols=cols, other_table_name=other_table_name, primary_key=primary_key, upsert_fields=upsert_fields)
@@ -87,14 +87,19 @@ class DOBIssuedPermit(BaseDatasetModel, models.Model):
     # Join DOBPermitIssuedLegacy table with DOBPermitIssuedNow table
     @classmethod
     def seed_or_update_self(self, **kwargs):
-        logger.info("Seeding/Updating {}", self.__name__)
+        logger.info("Starting seeding/updating process for {}", self.__name__)
+        
         # Add records from both tables
         legacy_table = ds.DOBPermitIssuedLegacy
         legacy_count = legacy_table.objects.count()
+        logger.info("Found {} records in legacy table", legacy_count)
+        
         legacy_cols = "concat({other_table_name}.job, {other_table_name}.permitsino), {other_table_name}.job, {other_table_name}.permitsino, {other_table_name}.bbl, {other_table_name}.bin, {other_table_name}.borough, {other_table_name}.house, {other_table_name}.streetname, {other_table_name}.worktype, {other_table_name}.jobtype, {other_table_name}.issuancedate, {other_table_name}.expirationdate, concat_ws(' ', {other_table_name}.permitteesfirstname, {other_table_name}.permitteeslastname), {other_table_name}.permitteesbusinessname, concat_ws(' ', {other_table_name}.ownersfirstname, {other_table_name}.ownerslastname), {other_table_name}.ownersbusinessname, {other_table_name}.id, \'{other_model_name}\', {other_table_name}.permittype, {other_table_name}.permitsubtype, {other_table_name}.permitstatus, {other_table_name}.filingstatus".format(other_table_name=legacy_table._meta.db_table, other_model_name=legacy_table._meta.model_name)
 
         now_table = ds.DOBPermitIssuedNow
         now_count = now_table.objects.count()
+        logger.info("Found {} records in now table", now_count)
+        
         now_cols = """
         SELECT DISTINCT ON ({other_table_name}.jobfilingnumber)
             concat({other_table_name}.jobfilingnumber, {other_table_name}.workpermit),
@@ -125,33 +130,41 @@ class DOBIssuedPermit(BaseDatasetModel, models.Model):
 
         kwargs['update'].total_rows = legacy_count + now_count
         kwargs['update'].save()
+        logger.info("Total records to process: {}", kwargs['update'].total_rows)
 
         starting_count = self.objects.count()
+        logger.info("Starting legacy table import. Current count: {}", starting_count)
         execute(self.upsert_permit_sql(legacy_table, legacy_cols))
         rows_created_legacy = self.objects.count() - starting_count
-        kwargs['update'].rows_created = kwargs['update'].rows_created + \
-            rows_created_legacy
-        kwargs['update'].rows_updated = kwargs['update'].rows_updated + \
-            (legacy_count - rows_created_legacy)
+        logger.info("Legacy import complete. Created: {}, Updated: {}", 
+                   rows_created_legacy, legacy_count - rows_created_legacy)
+        
+        kwargs['update'].rows_created = kwargs['update'].rows_created + rows_created_legacy
+        kwargs['update'].rows_updated = kwargs['update'].rows_updated + (legacy_count - rows_created_legacy)
         kwargs['update'].save()
-        logger.debug("Completed seed into {} for {}",
-                     self.__name__, legacy_table._meta.db_table)
 
         starting_count = self.objects.count()
+        logger.info("Starting now table import. Current count: {}", starting_count)
         execute(self.upsert_permit_sql(now_table, now_cols, raw_select=True))
         rows_created_now = self.objects.count() - starting_count
-        kwargs['update'].rows_created = kwargs['update'].rows_created + \
-            rows_created_now
-        kwargs['update'].rows_updated = kwargs['update'].rows_updated + \
-            (now_count - rows_created_now)
+        logger.info("Now import complete. Created: {}, Updated: {}", 
+                   rows_created_now, now_count - rows_created_now)
+        
+        kwargs['update'].rows_created = kwargs['update'].rows_created + rows_created_now
+        kwargs['update'].rows_updated = kwargs['update'].rows_updated + (now_count - rows_created_now)
         kwargs['update'].save()
-
-        logger.debug("Completed seed into {} for {}",
-                     self.__name__, now_table._meta.db_table)
 
         dataset = self.get_dataset()
         dataset.api_last_updated = datetime.today()
         dataset.save()
+        
+        final_count = self.objects.count()
+        logger.info("✅ Seeding complete for {}. Final counts:", self.__name__)
+        logger.info("📊 Total records: {}", final_count)
+        logger.info("📊 Records created: {}", kwargs['update'].rows_created)
+        logger.info("📊 Records updated: {}", kwargs['update'].rows_updated)
+        logger.info("📊 Legacy records: {}", legacy_count)
+        logger.info("📊 Now records: {}", now_count)
 
     @classmethod
     def annotate_properties(self):
